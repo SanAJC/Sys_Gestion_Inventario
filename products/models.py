f10a5f8df8bed0f521ab4609397677fa9d984e06
san ats <san@gmail.com>
2025-05-11 15:30:45 -0500

    Add imagenes en el modelo productos

--- products/models.py
+++ products/models.py
@@ -13,6 +13,7 @@ class Proveedor(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.IntegerField()
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
     
